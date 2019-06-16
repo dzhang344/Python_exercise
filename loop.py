759d923539e0030ace22a799b167d3f837586392
David Zhang <dzhang344@gmail.com>
2019-06-16 10:03:28 -0500

    fix code

--- loop.py
+++ loop.py
@@ -1,11 +1,13 @@
-number = ['1', '2', '3']
-dup = set()
+number = [1, 2, 3,4,5]
+dup = []
+count = 0
 for x in number:
     for y in number:
         for z in number:
-            newNum = x + y + z
-            if newNum not in dup:
-                dup.add(newNum)
+            if x != y and y != z:
+                newNum = 100*x + 10*y + z
+                dup.append(newNum)
+                count += 1
+print(len(dup), dup)
 
-for num in dup:
-    print(num)
+# print('total number is %d', %count)
