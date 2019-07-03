8d932b1224fabbdd9cdf9292a71ef1b77596b406
David Zhang <dzhang344@gmail.com>
2019-07-02 22:13:10 -0500

    spider practise

--- SpiderExercise/Chapter3/urlParseExample.py
+++ SpiderExercise/Chapter3/urlParseExample.py
@@ -28,3 +28,42 @@ from urllib.parse import urlunsplit
 # parameter must be 5
 data = ['http', 'www.baidu.com', 'index.html;user', 'a=6', 'comment']
 print(urlunsplit(data))
+
+
+from urllib.parse import urljoin
+
+print(urljoin('http://www.baidu.com', 'FAQ.html'))
+print(urljoin('http://www.baidu.com', 'https://cuiqingcai.com/FAQ.html'))
+
+from urllib.parse import urlencode
+
+params = {
+    'name': 'germey',
+    'age': 22
+}
+
+base_url = 'http://www.baidu.com?'
+url = base_url + urlencode(params)
+print(url)
+
+from urllib.parse import parse_qs
+
+query = 'name=germey&age=22'
+print(parse_qs(query))
+
+
+from urllib.parse import parse_qsl
+
+query = 'name=germey&age=22'
+print(parse_qsl(query))
+
+
+from urllib.parse import quote
+# convert word to url code
+keyword = '壁纸'
+url = 'https://www.baidu.com/s?wd=' + quote(keyword)
+print(url)
+
+from urllib.parse import unquote
+url = 'https://www.baidu.com/s?wd=%E5%A3%81%E7%BA%B8'
+print(unquote(url))
